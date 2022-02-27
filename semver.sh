db1ba9d05ecfbefc286de021bb1fbd6c32997ffa
Jarosław Dutka <jaroslawjdutka@gmail.com>
2022-02-27 21:22:19 +0100

    Add old and new tag to output

--- semver.sh
+++ semver.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-old_version=$(git describe --abbrev=1 --tags)
+old_tag=$(git describe --abbrev=1 --tags)
 
-if [[ $old_version =~ (v?)([0-9]+)\.([0-9]+)\.([0-9]+) ]]; 
+if [[ $old_tag =~ (v?)([0-9]+)\.([0-9]+)\.([0-9]+) ]]; 
 then 
   prefix=${BASH_REMATCH[1]}
   major=${BASH_REMATCH[2]}
   minor=${BASH_REMATCH[3]}
   patch=${BASH_REMATCH[4]}
 else 
-  echo "Invalid tag $old_version"
+  echo "Invalid tag $old_tag"
 fi
 
-echo "Current tag: $old_version"
+old_version="$major.$minor.$patch"
 
 message=$(git log -n 1 HEAD --format=%B)
 
@@ -27,17 +27,21 @@ else
   ((patch++))
 fi
 
-new_version="$prefix$major.$minor.$patch"
+new_tag="$prefix$major.$minor.$patch"
+new_version="$major.$minor.$patch"
 
-git tag $new_version
+git tag $new_tag
 
 if [ $? -eq 0 ]; then
-  echo "New tag: $new_version"
+  echo "Current tag: $old_tag"
+  echo "New tag: $new_tag"
+  echo "::set-output name=old_tag::$old_tag"
   echo "::set-output name=old_version::$old_version"
+  echo "::set-output name=new_tag::$new_tag"
   echo "::set-output name=new_version::$new_version"
   exit 0
 else
-  echo "Cannot create new tag: $new_version"
+  echo "Cannot create new tag: $new_tag"
   exit 1
 fi
 
